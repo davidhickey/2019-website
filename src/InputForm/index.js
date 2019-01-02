0d0360070badd1e979944c0f3ec21c265289f690
davidhickey <davidhickey423@gmail.com>
2019-01-02 14:31:29 -0500

    adds cards to portfolio.js

--- src/InputForm/index.js
+++ src/InputForm/index.js
@@ -3,6 +3,8 @@ import { Formik } from "formik";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Form } from "./form";
 import Paper from "@material-ui/core/Paper";
+import * as Yup from "yup";
+
 
 const styles = theme => ({
  paper: {
@@ -18,6 +20,21 @@ const styles = theme => ({
  }
 });
 
+const validationSchema = Yup.object({
+name: Yup.string("Enter a name")
+.required("Name is required"),
+email: Yup.string("Enter your email")
+.email("Enter a valid email")
+.required("Email is required"),
+password: Yup.string("")
+.min(8, "Password must contain at least 8 characters")
+.required("Enter your password"),
+confirmPassword: Yup.string("Enter your password")
+.required("Confirm your password")
+.oneOf([Yup.ref("password")], "Password does not match")
+});
+
+
 class InputForm extends Component {
  constructor(props) {
    super(props);
@@ -25,20 +42,24 @@ class InputForm extends Component {
  }
 
  render() {
-   const classes = this.props;
-   return (
-     <React.Fragment>
-          <div className={classes.container}>
-         <Paper elevation={1} className={classes.paper}>
-           <h1>Form</h1>
-           <Formik
-             render={props => <Form {...props} />}
-           />
-         </Paper>
-       </div>
-     </React.Fragment>
-   );
- }
+  const classes = this.props;
+  const values = { name: "", email: "", confirmPassword: "", password: "" };
+  return (
+<React.Fragment>
+    <div className={classes.container}>
+        <Paper elevation={1} className={classes.paper}>
+        <h1>Form</h1>
+        <Formik
+            render={props => <Form {...props} />}
+            initialValues={values}
+            validationSchema={validationSchema}
+        />
+        </Paper>
+    </div>
+</React.Fragment>
+);
+}
+
 }
 
 export default withStyles(styles)(InputForm);
